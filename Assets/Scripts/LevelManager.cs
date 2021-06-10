45d7dc31cce361af2d5b5eed7b56ed305293536c
pebro18 <pebro06@gmail.com>
2021-06-10 13:02:29 +0200

    Made basic levelmanager script for scene transitions

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadNextScene(string LevelName)
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+}
