8393aa41b48725a02372309656fb565a441119b0
pebro18 <pebro06@gmail.com>
2021-06-15 19:17:34 +0200

    Added Editor script for ReadAndCalculate

---
+++ Assets/Scripts/Editor/CalculatePathEditor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ReadAndCalculatePath))]
+public class CalculatePathEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ReadAndCalculatePath PathCalculate = (ReadAndCalculatePath)target;
+        if (GUILayout.Button("Regen LineRenderer"))
+        {
+            PathCalculate.LoadPositions();
+        }
+    }
+}
