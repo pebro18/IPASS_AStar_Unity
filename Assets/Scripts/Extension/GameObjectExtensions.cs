6d5c52a4e56f769310408edde0856ab48e607f13
pebro18 <pebro06@gmail.com>
2021-06-15 17:05:56 +0200

    Added Extension method for gameobject objects

---
+++ Assets/Scripts/Extension/GameObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameObjectExtensions
+{
+    public static class GameObjectExtensions
+    {
+        public static GameObject[] GetChildsObjs(this GameObject obj)
+        {
+            List<GameObject> Childs = new List<GameObject>();
+
+            for (int i = 0; i < obj.transform.childCount; i++)
+            {
+                Childs.Add(obj.transform.GetChild(i).gameObject);
+            }
+            
+            return Childs.ToArray();
+
+        }
+    }
+}
